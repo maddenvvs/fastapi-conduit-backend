de4174c4fa96f8d4918d729526e105d903e84150
maddenvvs <maddenvvs@gmail.com>
2025-02-05 08:32:12 +0000

    Move tags to the router

--- conduit/api/routes/tags.py
+++ conduit/api/routes/tags.py
@@ -3,13 +3,14 @@ from fastapi import APIRouter
 from conduit.api.contract.responses.tags import GetAllTagsApiResponse
 from conduit.api.dependencies import ITagsService
 
-router = APIRouter()
+router = APIRouter(
+    tags=["Tags"],
+)
 
 
 @router.get(
     path="/tags",
     response_model=GetAllTagsApiResponse,
-    tags=["Tags"],
 )
 async def get_all_tags(tags_service: ITagsService) -> GetAllTagsApiResponse:
     tags = await tags_service.get_all_tags()
