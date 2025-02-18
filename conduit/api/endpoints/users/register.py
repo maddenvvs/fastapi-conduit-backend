cb501c415780df4b1b07271af7f8b184f5d96b5f
maddenvvs <maddenvvs@gmail.com>
2025-02-18 17:05:33 +0000

    Add email validation for registering user

--- conduit/api/endpoints/users/register.py
+++ conduit/api/endpoints/users/register.py
@@ -1,22 +1,35 @@
-from typing import Optional
+from typing import Annotated, Optional, final
 
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Depends
-from pydantic import BaseModel
+from fastapi import APIRouter, Body, Depends
+from pydantic import BaseModel, EmailStr, Field
+from typing_extensions import Self
 
 from conduit.containers import Container
 from conduit.domain.entities.users import RegisteredUser, RegisterUserDetails
 from conduit.domain.use_cases.register_user.use_case import RegisterUserUseCase
 
 
+@final
 class RegisterUserData(BaseModel):
-    username: str
-    email: str
-    password: str
+    username: str = Field(
+        description="User name used in a profile. Should be unique among other usernames.",
+        examples=["walkmansit"],
+        min_length=1,
+    )
+    email: EmailStr = Field(
+        description="Email address. Should be unique among other emails.",
+    )
+    password: str = Field(
+        description="Password to be used during the login.",
+        examples=["use_your_own_password"],
+        min_length=1,
+    )
 
 
+@final
 class RegisterUserApiRequest(BaseModel):
-    user: RegisterUserData
+    user: RegisterUserData = Field(description="New user information.")
 
     def to_domain(self) -> RegisterUserDetails:
         user = self.user
@@ -27,19 +40,35 @@ class RegisterUserApiRequest(BaseModel):
         )
 
 
+@final
 class RegisteredUserData(BaseModel):
-    email: str
-    username: str
-    bio: str
-    image: Optional[str]
-    token: str
+    email: str = Field(
+        description="The email address of the registered user.",
+    )
+    username: str = Field(
+        description="The username of the registered user.",
+        examples=["walkmansit"],
+    )
+    bio: str = Field(
+        description="The biography information of the registered user (empty by default).",
+        examples=[""],
+    )
+    image: Optional[str] = Field(
+        description="The image URL of the registered user (null by default).",
+        examples=[None],
+    )
+    token: str = Field(
+        description="JSON Web Token (JWT) issued for the registered user.",
+        examples=["header.payload.signature"],
+    )
 
 
+@final
 class RegisterUserApiResponse(BaseModel):
     user: RegisteredUserData
 
     @classmethod
-    def from_domain(cls, user: RegisteredUser) -> "RegisterUserApiResponse":
+    def from_domain(cls, user: RegisteredUser) -> Self:
         return cls(
             user=RegisteredUserData(
                 username=user.username,
@@ -58,10 +87,14 @@ router = APIRouter()
     path="/users",
     response_model=RegisterUserApiResponse,
     tags=["Users"],
+    summary="Register a new user",
 )
 @inject
 async def register_user(
-    request: RegisterUserApiRequest,
+    request: Annotated[
+        RegisterUserApiRequest,
+        Body(),
+    ],
     register_user: RegisterUserUseCase = Depends(
         Provide[Container.register_user_use_case]
     ),
