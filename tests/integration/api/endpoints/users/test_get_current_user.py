a01165119d684587bf20edafce3c2e602ef5a493
maddenvvs <maddenvvs@gmail.com>
2025-02-24 16:53:07 +0000

    Fix naming in get current user tests

--- tests/integration/api/endpoints/users/test_get_current_user.py
+++ tests/integration/api/endpoints/users/test_get_current_user.py
@@ -27,22 +27,26 @@ class TestWhenVisitingAnonymously:
 class TestWhenVisitingByRegisteredUser:
 
     @pytest.fixture
-    async def anonymous_response(self, registered_user_client: AsyncClient) -> Response:
+    async def registered_user_response(
+        self, registered_user_client: AsyncClient
+    ) -> Response:
         response = await registered_user_client.get("/user")
         return response
 
     @pytest.mark.anyio
-    async def test_returns_status_200_OK(self, anonymous_response: Response) -> None:
-        assert anonymous_response.status_code == 200
+    async def test_returns_status_200_OK(
+        self, registered_user_response: Response
+    ) -> None:
+        assert registered_user_response.status_code == 200
 
     @pytest.mark.anyio
-    async def test_returns_empty_body(
+    async def test_returns_user_details_with_token(
         self,
-        anonymous_response: Response,
+        registered_user_response: Response,
         registered_user: UserModel,
         registered_user_token: str,
     ) -> None:
-        assert anonymous_response.json() == {
+        assert registered_user_response.json() == {
             "user": {
                 "email": registered_user.email,
                 "username": registered_user.username,
