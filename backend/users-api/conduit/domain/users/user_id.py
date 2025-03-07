a85f389157c7b33eb034060d3911d1b52fb5f52f
maddenvvs <maddenvvs@gmail.com>
2025-03-07 14:47:47 +0000

    users-api: add slots to user id value object

--- backend/users-api/conduit/domain/users/user_id.py
+++ backend/users-api/conduit/domain/users/user_id.py
@@ -7,6 +7,8 @@ from conduit.domain.users.errors import Errors
 
 @final
 class UserId(int):
+    __slots__ = ()
+
     def __new__(cls, value: Any) -> Self:
         if type(value) is not int:
             raise Errors.invalid_user_id(value)
